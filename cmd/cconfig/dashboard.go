de48c4b881200c8b923a1da30f96257e961547cf
frank12268 <zihong.lv@gmail.com>
2017-01-19 17:27:45 +0800

    add codis-ha functionality to dashboard

--- cmd/cconfig/dashboard.go
+++ cmd/cconfig/dashboard.go
@@ -26,6 +26,7 @@ import (
 	"github.com/CodisLabs/codis/pkg/utils/errors"
 	"github.com/CodisLabs/codis/pkg/utils/log"
 
+	"github.com/garyburd/redigo/redis"
 	"strings"
 )
 
@@ -167,6 +168,111 @@ func releaseDashboardNode() {
 	//}
 }
 
+type redisChecker struct {
+	addr           string
+	defaultTimeout time.Duration
+}
+
+func (r *redisChecker) ping() error {
+	c, err := redis.DialTimeout("tcp", r.addr, r.defaultTimeout, r.defaultTimeout, r.defaultTimeout)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+	_, err = c.Do("ping")
+	return err
+}
+
+func (r *redisChecker) CheckAlive() error {
+	var err error
+	for i := 0; i < 7; i++ { //try a few times
+		err = r.ping()
+		if err != nil {
+			time.Sleep(3 * time.Second)
+			continue
+		}
+		return nil
+	}
+	return err
+}
+
+func PingServer(checker redisChecker, errCtx interface{}, errCh chan<- interface{}) {
+	err := checker.CheckAlive()
+	log.Debugf("check %+v, result:%v, errCtx:%+v", checker, err, errCtx)
+	if err != nil {
+		errCh <- errCtx
+		return
+	}
+	errCh <- nil
+}
+
+func getSlave(master *models.Server) (*models.Server, error) {
+	group, err := models.GetGroup(safeZkConn, globalEnv.ProductName(), master.GroupId)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	for _, s := range group.Servers {
+		if s.Type == models.SERVER_TYPE_SLAVE {
+			return s, nil
+		}
+	}
+	return nil, errors.Errorf("can not find any slave in this group: %v", group)
+}
+
+func handleCrashedServer(s *models.Server) error {
+	switch s.Type {
+	case models.SERVER_TYPE_MASTER:
+		//get slave and do promote
+		slave, err := getSlave(s)
+		if err != nil {
+			log.ErrorErrorf(err, "get slave failed")
+			return err
+		}
+		log.Infof("try promote %+v", slave)
+		code, err0 := apiPromoteServer(*slave, nil)
+		if code!=200 {
+			log.Errorf("do promote %v failed : %v", slave, err0)
+			return err
+		}
+	case models.SERVER_TYPE_SLAVE:
+		log.Errorf("slave is down: %+v", s)
+	case models.SERVER_TYPE_OFFLINE:
+		//no need to handle it
+	default:
+		log.Errorf("unkonwn type %+v", s)
+	}
+	return nil
+}
+
+//ping codis-server find crashed codis-server
+func CheckAliveAndPromote(groups []*models.ServerGroup) ([]models.Server, error) {
+	errCh := make(chan interface{}, 100)
+	var serverCnt int
+	for _, group := range groups { //each group
+		for _, s := range (*group).Servers { //each server
+			serverCnt++
+			rc := redisChecker{addr : s.Addr, defaultTimeout : 5*time.Second}
+			news := s
+			go PingServer(rc, news, errCh)
+		}
+	}
+	//get result
+	var crashedServer []models.Server
+	for i := 0; i < serverCnt; i++ {
+		s := <-errCh
+		if s == nil { //alive
+			continue
+		}
+		log.Warnf("server maybe crashed %+v", s)
+		crashedServer = append(crashedServer, *s.(*models.Server))
+		err := handleCrashedServer(s.(*models.Server))
+		if err != nil {
+			return crashedServer, err
+		}
+	}
+	return crashedServer, nil
+}
+
 func runDashboard(addr string, httpLogFile string) {
 	log.Infof("dashboard listening on addr: %s", addr)
 	m := martini.Classic()
@@ -247,6 +353,18 @@ func runDashboard(addr string, httpLogFile string) {
 
 	// create long live migrate manager
 	globalMigrateManager = NewMigrateManager(safeZkConn, globalEnv.ProductName())
+
+	go func(){
+		for {
+			log.Info("Calling HA")
+			groups, err := models.ServerGroups(safeZkConn, globalEnv.ProductName())
+			if err != nil {
+				log.ErrorErrorf(err, "call ServerGroups failed")
+			}
+			CheckAliveAndPromote(groups)
+			time.Sleep(3 * time.Second)
+		}
+	}()
 
 	// dashboard keep alive
 	go func(){
